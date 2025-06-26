f191d10f4cbdefb301db1260b46c17864fd558d6
pedro-henrique-xavier <pedrohenpxavier@hotmail.com>
2025-06-26 13:33:59 -0300

    Ajuste no bootstrap de Livros

--- livro/forms.py
+++ livro/forms.py
@@ -1,7 +1,16 @@
 from .models import Livro
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput, Select, NumberInput
 
 class LivroForm(ModelForm):
     class Meta:
         model = Livro
         fields = ['nome', 'isbn', 'editora', 'ano_publicacao', 'quantidade', 'autor', 'categoria']
+        widgets = {
+            'nome': TextInput(attrs={'class': 'form-control'}),
+            'isbn': TextInput(attrs={'class': 'form-control'}),
+            'editora': TextInput(attrs={'class': 'form-control'}),
+            'ano_publicacao': NumberInput(attrs={'class': 'form-control'}),
+            'quantidade': NumberInput(attrs={'class': 'form-control'}),
+            'autor': Select(attrs={'class': 'form-select'}),
+            'categoria': Select(attrs={'class': 'form-select'}),
+        }
--- livro/urls.py
+++ livro/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.index, name='index-livro'),
     path('<int:id_livro>/', views.detalha, name='index-detalha'),
     path('create/', views.create, name='create-livro'),
-    path('update/<int:id_livro>', views.update, name='update-livro'),
-    path('delete/<int:id_livro>', views.delete, name='delete-livro')
+    path('update/<int:id_livro>/', views.update, name='update-livro'),
+    path('delete/<int:id_livro>/', views.delete, name='delete-livro')
 ]
